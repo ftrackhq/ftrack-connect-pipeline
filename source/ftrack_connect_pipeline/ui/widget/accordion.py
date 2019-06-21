09f08f67f6c1b937fc6cca2f85a1fe8da1805f06
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-06-21 12:26:54 +0200

    code cleanup

--- source/ftrack_connect_pipeline/ui/widget/accordion.py
+++ source/ftrack_connect_pipeline/ui/widget/accordion.py
@@ -11,13 +11,18 @@ class AccordionWidget(QtWidgets.QWidget):
         self._content, self._content_layout = (None, None)
 
         self._main_v_layout = QtWidgets.QVBoxLayout(self)
-        self._main_v_layout.addWidget(self.initTitleFrame(title, self._is_collasped))
-        self._main_v_layout.addWidget(self.initContent(self._is_collasped))
+
+        title_widget = self.initTitleFrame(title, self._is_collasped)
+        self._main_v_layout.addWidget(title_widget)
+
+        content_widget = self.initContent(self._is_collasped)
+        self._main_v_layout.addWidget(content_widget)
 
         self.initCollapsable()
 
     def initTitleFrame(self, title, collapsed):
-        self._title_frame = AccordionTitleWidget(title=title, collapsed=collapsed)
+        self._title_frame = AccordionTitleWidget(
+            title=title, collapsed=collapsed)
 
         return self._title_frame
 
@@ -34,7 +39,7 @@ class AccordionWidget(QtWidgets.QWidget):
         self._content_layout.addWidget(widget)
 
     def initCollapsable(self):
-        QtCore.QObject.connect(self._title_frame, QtCore.SIGNAL('clicked()'), self.toggleCollapsed)
+        self._title_frame.clicked.connect(self.toggleCollapsed)
 
     def toggleCollapsed(self):
         self._content.setVisible(self._is_collasped)
@@ -48,7 +53,7 @@ class AccordionTitleWidget(QtWidgets.QFrame):
 
         self.setMinimumHeight(24)
         self.move(QtCore.QPoint(24, 0))
-        # self.setStyleSheet("border:1px solid rgb(41, 41, 41); ")
+        self.setStyleSheet("border:1px solid rgb(41, 41, 41); ")
 
         self._hlayout = QtWidgets.QHBoxLayout(self)
         self._hlayout.setContentsMargins(0, 0, 0, 0)
@@ -62,7 +67,7 @@ class AccordionTitleWidget(QtWidgets.QFrame):
 
     def initArrow(self, collapsed):
         self._arrow = Arrow(collapsed=collapsed)
-        # self._arrow.setStyleSheet("border:0px")
+        self._arrow.setStyleSheet("border:0px")
 
         return self._arrow
 
@@ -70,12 +75,13 @@ class AccordionTitleWidget(QtWidgets.QFrame):
         self._title = QtWidgets.QLabel(title)
         self._title.setMinimumHeight(24)
         self._title.move(QtCore.QPoint(24, 0))
-        # self._title.setStyleSheet("border:0px")
+        self._title.setStyleSheet("border:0px")
 
         return self._title
 
     def mousePressEvent(self, event):
-        self.emit(QtCore.SIGNAL('clicked()'))
+        self.clicked.emit()
+        # self.emit(QtCore.SIGNAL('clicked()'))
 
         return super(AccordionTitleWidget, self).mousePressEvent(event)
 
@@ -88,11 +94,19 @@ class Arrow(QtWidgets.QFrame):
 
         # horizontal == 0
         self._arrow_horizontal = QtGui.QPolygonF().fromList(
-            [QtCore.QPointF(7.0, 8.0), QtCore.QPointF(17.0, 8.0), QtCore.QPointF(12.0, 13.0)]
+            [
+                QtCore.QPointF(7.0, 8.0),
+                QtCore.QPointF(17.0, 8.0),
+                QtCore.QPointF(12.0, 13.0)
+            ]
         )
         # vertical == 1
         self._arrow_vertical = QtGui.QPolygonF().fromList(
-            [QtCore.QPointF(8.0, 7.0), QtCore.QPointF(13.0, 12.0), QtCore.QPointF(8.0, 17.0)]
+            [
+                QtCore.QPointF(8.0, 7.0),
+                QtCore.QPointF(13.0, 12.0),
+                QtCore.QPointF(8.0, 17.0)
+            ]
         )
         # arrow
         self._arrow = None
