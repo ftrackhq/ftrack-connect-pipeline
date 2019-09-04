03ddd43c7a07bd7fa5181805b3d131dc226d3e47
l_angeli <lorenzo.angeli@ftrack.com>
2019-09-04 17:38:07 +0200

    remove threading

--- source/ftrack_connect_pipeline/event.py
+++ source/ftrack_connect_pipeline/event.py
@@ -58,9 +58,26 @@ class EventManager(object):
         '''Emit *event* and provide *callback* function.'''
 
         if not remote:
-            event_thread = _EventThread(self.session, event, callback)
-            event_thread.start()
+            # event_thread = _EventThread(self.session, event, callback)
+            # event_thread.start()
 
+            result = self.session.event_hub.publish(
+                event,
+                synchronous=True,
+            )
+
+            if result:
+                result = result[0]
+
+            # Mock async event reply.
+            result_event = ftrack_api.event.base.Event(
+                topic=u'ftrack.meta.reply',
+                data=result,
+                in_reply_to_event=event['id'],
+            )
+
+            if callback:
+                callback(result_event)
         else:
             self.session.event_hub.publish(
                 event,
