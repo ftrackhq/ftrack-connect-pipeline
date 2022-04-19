9fec0ee3e69c816760243130cf235c49a125e98d
Lluis Ftrack <lluis.casals@ftrack.com>
2022-04-19 13:03:19 +0200

    code tested and stable

--- source/ftrack_connect_pipeline/constants/asset/__init__.py
+++ source/ftrack_connect_pipeline/constants/asset/__init__.py
@@ -56,10 +56,6 @@ REFERENCE_OBJECT = 'reference_object'
 #: :class:`~ftrack_connect_pipeline.asset.asset_info.FtrackAssetInfo`
 #: and the DCC ftrack plugin.
 IS_LATEST_VERSION = 'is_latest_version'
-#: Session constant identifier key for ftrack assets connected or used with
-#: :class:`~ftrack_connect_pipeline.asset.asset_info.FtrackAssetInfo`
-#: and the DCC ftrack plugin.
-SESSION = 'session'
 #: Asset info ID constant identifier key for ftrack assets connected or used with
 #: :class:`~ftrack_connect_pipeline.asset.asset_info.FtrackAssetInfo`
 #: and the DCC ftrack plugin.
@@ -92,7 +88,6 @@ KEYS = [
     ASSET_INFO_OPTIONS,
     REFERENCE_OBJECT,
     IS_LATEST_VERSION,
-    SESSION,
     ASSET_INFO_ID,
     DEPENDENCY_IDS,
     IS_LOADED,
