1f5cd6b59793041f334a403361b3b5345cc23f21
Lluis Ftrack <lluis.casals@ftrack.com>
2022-09-21 13:17:39 +0200

    code clean and options bug fixed

--- source/ftrack_connect_pipeline/definition/definition_object.py
+++ source/ftrack_connect_pipeline/definition/definition_object.py
@@ -1,9 +1,6 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014-2022 ftrack
 
-import logging
-
-
 class DefinitionObject(dict):
     '''Base DccObject class.'''
 
@@ -16,27 +13,6 @@ class DefinitionObject(dict):
 
     _categories = {}
     '''Holds all the custom categories on the current definition'''
-
-    @property
-    def steps(self):
-        '''
-        Returns all the steps in definition
-        '''
-        return self._categories['step']
-
-    @property
-    def stages(self):
-        '''
-        Returns all the stages in definition
-        '''
-        return self._categories['stage']
-
-    @property
-    def plugins(self):
-        '''
-        Returns all the plugins in definition
-        '''
-        return self._categories['plugin']
 
     def get_by_name(self, name, type_name=None):
         '''
@@ -67,6 +43,12 @@ class DefinitionObject(dict):
                     if item.get('type') == type_name:
                         results.append(item)
         return results
+
+    def get_by_category(self, category_name):
+        '''
+        Returns all the objects with the given *category_name* in the definition.
+        '''
+        return self._categories.get(category_name)
 
     def __getattr__(self, item):
         return self[item]
@@ -101,7 +83,7 @@ class DefinitionObject(dict):
             if v.category in list(self._categories.keys()):
                 self._categories[v.category].extend(v)
             else:
-                self._categories[v.category] = [item for item in v]
+                self._categories[v.category] = list(item for item in v)
 
         # If dictionary and valid category, convert to category object
         elif issubclass(type(v), dict):
@@ -172,13 +154,14 @@ class Stage(DefinitionObject):
 
 class Plugin(DefinitionObject):
 
-    @property
-    def options(self):
+    def __setitem__(self, k, v):
         '''
-        Returns the attribute objects_loaded of the current
-        self :obj:`name`
+        Sets the given *v* into the given *k*
         '''
-        return Options(self.get('options'))
+        # Convert options to options object
+        if k == 'options':
+            v = Options(v)
+        super(DefinitionObject, self).__setitem__(k, v)
 
     def __init__(self, plugin):
         super(Plugin, self).__init__(plugin)
@@ -249,35 +232,3 @@ class DefinitionList(list):
                     self.category = category
         return item
 
-
-
-
-'''
-Test:
-from ftrack_connect_pipeline.definition import definition_object
-definition = {'type': 'publisher', 'name': 'File Publisher', 'asset_type': 'scene', 'task_type': 'animation', 'host_type': 'python', 'ui_type': '', 'discoverable': ['Task'], 'contexts': [{'category': 'step', 'type': 'context', 'name': 'main', 'optional': False, 'enabled': True, 'selected': True, 'stages': [{'category': 'stage', 'type': 'context', 'visible': True, 'name': 'context', 'plugins': [{'category': 'plugin', 'type': 'context', 'name': 'context selector', 'plugin': 'common_default_publisher_context', 'widget': 'common_default_publisher_context', 'visible': True, 'editable': True, 'options': {'context_id': '15b346c0-022e-11ea-bc02-ae185fc6f561', 'asset_name': 'testFromPythonConsole', 'comment': 'Nothing to comment', 'status_id': '44dd9fb2-4164-11df-9218-0019bb4983d8'}, 'default_method': 'run'}]}], 'stage_order': ['context']}], 'components': [{'category': 'step', 'type': 'component', 'name': 'main', 'optional': False, 'enabled': True, 'selected': True, 'stages': [{'category': 'stage', 'type': 'collector', 'visible': True, 'name': 'collector', 'plugins': [{'category': 'plugin', 'type': 'collector', 'name': 'collect from given path', 'plugin': 'common_default_publisher_collector', 'widget': 'common_default_publisher_collector', 'visible': True, 'editable': True, 'options': {'path': '/Users/ftrack/Desktop/Screenshot 2022-03-24 at 12.19.03.png'}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'validator', 'visible': True, 'name': 'validator', 'plugins': [{'category': 'plugin', 'type': 'validator', 'name': 'file exists', 'plugin': 'common_file_exists_publisher_validator', 'visible': True, 'editable': True, 'options': {}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'exporter', 'visible': True, 'name': 'exporter', 'plugins': [{'category': 'plugin', 'type': 'exporter', 'name': 'passthrough exporter', 'plugin': 'common_passthrough_publisher_exporter', 'visible': True, 'editable': True, 'options': {}, 'default_method': 'run'}]}], 'stage_order': ['collector', 'validator', 'exporter']}, {'category': 'step', 'type': 'component', 'name': 'thumbnail', 'optional': True, 'enabled': True, 'selected': True, 'stages': [{'category': 'stage', 'type': 'collector', 'visible': True, 'name': 'collector', 'plugins': [{'category': 'plugin', 'type': 'collector', 'name': 'collect from given path', 'plugin': 'common_default_publisher_collector', 'widget': 'common_default_publisher_collector', 'visible': True, 'editable': True, 'options': {'path': '/Users/ftrack/Desktop/Screenshot 2022-03-24 at 12.19.03.png'}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'validator', 'visible': True, 'name': 'validator', 'plugins': [{'category': 'plugin', 'type': 'validator', 'name': 'file exists', 'plugin': 'common_file_exists_publisher_validator', 'visible': True, 'editable': True, 'options': {}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'exporter', 'visible': True, 'name': 'exporter', 'plugins': [{'category': 'plugin', 'type': 'exporter', 'name': 'passthrough exporter', 'plugin': 'common_passthrough_publisher_exporter', 'visible': True, 'editable': True, 'options': {}, 'default_method': 'run'}]}], 'stage_order': ['collector', 'validator', 'exporter']}], 'finalizers': [{'category': 'step', 'type': 'finalizer', 'name': 'main', 'optional': False, 'enabled': True, 'selected': True, 'stages': [{'category': 'stage', 'type': 'pre_finalizer', 'visible': True, 'name': 'pre_finalizer', 'plugins': [{'category': 'plugin', 'type': 'pre_finalizer', 'name': 'pre to ftrack server', 'plugin': 'common_default_publisher_pre_finalizer', 'visible': False, 'editable': True, 'options': {}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'finalizer', 'visible': True, 'name': 'finalizer', 'plugins': [{'category': 'plugin', 'type': 'finalizer', 'name': 'to ftrack server', 'plugin': 'common_default_publisher_finalizer', 'visible': False, 'editable': True, 'options': {}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'post_finalizer', 'visible': True, 'name': 'post_finalizer', 'plugins': [{'category': 'plugin', 'type': 'post_finalizer', 'name': 'post to ftrack server', 'plugin': 'common_default_publisher_post_finalizer', 'visible': False, 'editable': True, 'options': {}, 'default_method': 'run'}]}], 'stage_order': ['pre_finalizer', 'finalizer', 'post_finalizer']}], '_config': {'type': 'config', 'engine_type': 'publisher'}}
-def_object = definition_object.DefinitionObject(definition)
-steps = def_object.steps
-plugin = steps[0].stages[0].plugins[0]
-options = plugin.options
-options.test
-options.keys()
-options.asset_name
-options.asset_name = 'testing'
-
-
-#doing it by our custom class the dictionary is not muttable
-from ftrack_connect_pipeline.definition import definition_object
-a = {'a': 1, 'context':[{'category':'step', 'info': [{'category':'stage','info': [{'a': 4}]}]}]}
-tt = definition_object.DefinitionObject(a)
-st = tt.steps[0]
-st['info']='qwe'
-tt
-
-
-#Doing it manually the dictionary is mutable
-a = {'a': 1, 'context':[{'category':'step', 'info': [{'category':'stage','info': [{'a': 4}]}]}]}
-b = a['context'][0]
-b['info']='qwe'
-a
--'''
