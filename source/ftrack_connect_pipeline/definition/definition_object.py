04b89777d4f2d4c6d5e10a66bef35ad9bbf21d4b
Lluis Ftrack <lluis.casals@ftrack.com>
2022-09-21 12:12:53 +0200

    definition object and definition list first implementation working

--- source/ftrack_connect_pipeline/definition/definition_object.py
+++ source/ftrack_connect_pipeline/definition/definition_object.py
@@ -2,7 +2,6 @@
 # :copyright: Copyright (c) 2014-2022 ftrack
 
 import logging
-from ftrack_connect_pipeline.constants import asset as asset_const
 
 
 class DefinitionObject(dict):
@@ -11,7 +10,7 @@ class DefinitionObject(dict):
     # TODO: could be useful to have a unique id for each definition?
     definition_id = None
     '''Definition id used to unique identify definitions. Not implemented yet '''
-    # Definie valid categories that can be converted to custom dictionaries
+
     valid_categories = ['step', 'stage', 'plugin']
     '''Definie valid categories that can be converted to custom dictionaries'''
 
@@ -39,6 +38,36 @@ class DefinitionObject(dict):
         '''
         return self._categories['plugin']
 
+    def get_by_name(self, name, type_name=None):
+        '''
+        Returns all the objects with the given *name* in the definition.
+        *type_name* can optionally be passed to filter by name and type at the
+        same time.
+        '''
+        results = []
+        for k, v in self._categories.items():
+            for item in v:
+                if issubclass(type(item), dict):
+                    if item.get('name') == name:
+                        if not type_name:
+                            results.append(item)
+                            continue
+                        if item.get('type') == type_name:
+                            results.append(item)
+        return results
+
+    def get_by_type(self, type_name):
+        '''
+        Returns all the objects with the given *type_name* in the definition.
+        '''
+        results = []
+        for k, v in self._categories.items():
+            for item in v:
+                if issubclass(type(item), dict):
+                    if item.get('type') == type_name:
+                        results.append(item)
+        return results
+
     def __getattr__(self, item):
         return self[item]
 
@@ -47,13 +76,8 @@ class DefinitionObject(dict):
 
     def __init__(self, definition, **kwargs):
         '''
-        If the *from_id* is provided find an object in the dcc with the given
-        *from_id* as assset_info_id.
-        If a *name* is provided create a new object in the dcc.
+        Convert the given definition to a DefinitionObject
         '''
-        # self.logger = logging.getLogger(
-        #     '{0}.{1}'.format(__name__, self.__class__.__name__)
-        # )
         super(DefinitionObject, self).__init__({}, **kwargs)
         for key, value in definition.items():
             self[key] = value
@@ -73,7 +97,11 @@ class DefinitionObject(dict):
         # If list convert to definition list
         if type(v) == list:
             v = DefinitionList(v)
-            self._categories[v.category] = v
+            # Save and clasify the list object to it's category to quickly query them on the properties
+            if v.category in list(self._categories.keys()):
+                self._categories[v.category].extend(v)
+            else:
+                self._categories[v.category] = [item for item in v]
 
         # If dictionary and valid category, convert to category object
         elif issubclass(type(v), dict):
@@ -96,7 +124,7 @@ class DefinitionObject(dict):
         '''
         Updates the current keys and values with the given ones.
         '''
-        # We override this method to make sure that we the values are updated
+        # We override this method to make sure that the values are updated
         # using the __setitem__ method
         if args:
             if len(args) > 1:
@@ -118,28 +146,11 @@ class DefinitionObject(dict):
             self[key] = value
         return self[key]
 
-    # @staticmethod
-    # def get_category(item, category_type):
-    #     print(type(item))
-    #     category_items = DefinitionList([])
-    #     if issubclass(type(item), dict):
-    #         category = item.get('category')
-    #         if category:
-    #             if category == category_type:
-    #                 return item
-    #         for value in list(item.values()):
-    #             if issubclass(type(value), list):
-    #                 for val in value:
-    #                     fund_item = DefinitionObject.get_category(val, category_type)
-    #                     if fund_item:
-    #                         #return fund_item
-    #                         category_items.append(fund_item)
-    #     if category_items:
-    #         return category_items
-
     def evaluate_item(self, item):
-        # Make sure item is converted to custom object if it's from a
-        # compatible category
+        '''
+        Make sure item is converted to custom object if it's from a
+        compatible category
+        '''
         if issubclass(type(item), dict):
             category = item.get('category')
             if category:
@@ -147,8 +158,6 @@ class DefinitionObject(dict):
                     cls = eval(category.capitalize())
                     item = cls(item)
         return item
-
-
 
 
 class Step(DefinitionObject):
@@ -174,35 +183,27 @@ class Plugin(DefinitionObject):
     def __init__(self, plugin):
         super(Plugin, self).__init__(plugin)
 
-    #TODO: maybe create the options class as well in order to properly
-    # augn¡ment the options whichis the important peace.
-    #  Is ther a way to convert all the dictonary keys to properties?
 
 class Options(DefinitionObject):
 
-    @property
-    def test(self):
-        '''
-        Returns the attribute objects_loaded of the current
-        self :obj:`name`
-        '''
-        return 'test'
     def __init__(self, options):
         super(Options, self).__init__(options)
 
-    #TODO: maybe create the options class as well in order to properly
-    # augn¡ment the options whichis the important peace.
-    #  Is ther a way to convert all the dictonary keys to properties?
-
 
 class DefinitionList(list):
-    # Define valid categories that can be converted to custom dictionaries
     valid_categories = ['step', 'stage', 'plugin']
+    '''Definie valid categories that can be converted to custom dictionaries'''
+
+    # We use the category to identify the type of definition list in
+    # the definition object
     category = None
+    '''Category of the definition list given by the definition'''
 
     def __init__(self, iterable):
-        # Iterate over all the objects in the given iterable list, and convert
-        # them to custom objects if they match a compatible category
+        '''
+        Iterate over all the objects in the given *iterable* list, and convert
+        them to custom objects if they match a compatible category
+        '''
         new_iter = []
         for item in iterable:
             # evaluate item before assign it
@@ -234,13 +235,49 @@ class DefinitionList(list):
         super(DefinitionList, self).extend(new_iter)
 
     def evaluate_item(self, item):
-        # Make sure item is converted to custom object if it's from a
-        # compatible category
+        '''
+        Make sure item is converted to custom object if it's from a
+        compatible category
+        '''
         if issubclass(type(item), dict):
             category = item.get('category')
             if category:
                 if category in self.valid_categories:
                     cls = eval(category.capitalize())
                     item = cls(item)
+                    # Set up the category of the list
                     self.category = category
         return item
+
+
+
+
+'''
+Test:
+from ftrack_connect_pipeline.definition import definition_object
+definition = {'type': 'publisher', 'name': 'File Publisher', 'asset_type': 'scene', 'task_type': 'animation', 'host_type': 'python', 'ui_type': '', 'discoverable': ['Task'], 'contexts': [{'category': 'step', 'type': 'context', 'name': 'main', 'optional': False, 'enabled': True, 'selected': True, 'stages': [{'category': 'stage', 'type': 'context', 'visible': True, 'name': 'context', 'plugins': [{'category': 'plugin', 'type': 'context', 'name': 'context selector', 'plugin': 'common_default_publisher_context', 'widget': 'common_default_publisher_context', 'visible': True, 'editable': True, 'options': {'context_id': '15b346c0-022e-11ea-bc02-ae185fc6f561', 'asset_name': 'testFromPythonConsole', 'comment': 'Nothing to comment', 'status_id': '44dd9fb2-4164-11df-9218-0019bb4983d8'}, 'default_method': 'run'}]}], 'stage_order': ['context']}], 'components': [{'category': 'step', 'type': 'component', 'name': 'main', 'optional': False, 'enabled': True, 'selected': True, 'stages': [{'category': 'stage', 'type': 'collector', 'visible': True, 'name': 'collector', 'plugins': [{'category': 'plugin', 'type': 'collector', 'name': 'collect from given path', 'plugin': 'common_default_publisher_collector', 'widget': 'common_default_publisher_collector', 'visible': True, 'editable': True, 'options': {'path': '/Users/ftrack/Desktop/Screenshot 2022-03-24 at 12.19.03.png'}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'validator', 'visible': True, 'name': 'validator', 'plugins': [{'category': 'plugin', 'type': 'validator', 'name': 'file exists', 'plugin': 'common_file_exists_publisher_validator', 'visible': True, 'editable': True, 'options': {}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'exporter', 'visible': True, 'name': 'exporter', 'plugins': [{'category': 'plugin', 'type': 'exporter', 'name': 'passthrough exporter', 'plugin': 'common_passthrough_publisher_exporter', 'visible': True, 'editable': True, 'options': {}, 'default_method': 'run'}]}], 'stage_order': ['collector', 'validator', 'exporter']}, {'category': 'step', 'type': 'component', 'name': 'thumbnail', 'optional': True, 'enabled': True, 'selected': True, 'stages': [{'category': 'stage', 'type': 'collector', 'visible': True, 'name': 'collector', 'plugins': [{'category': 'plugin', 'type': 'collector', 'name': 'collect from given path', 'plugin': 'common_default_publisher_collector', 'widget': 'common_default_publisher_collector', 'visible': True, 'editable': True, 'options': {'path': '/Users/ftrack/Desktop/Screenshot 2022-03-24 at 12.19.03.png'}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'validator', 'visible': True, 'name': 'validator', 'plugins': [{'category': 'plugin', 'type': 'validator', 'name': 'file exists', 'plugin': 'common_file_exists_publisher_validator', 'visible': True, 'editable': True, 'options': {}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'exporter', 'visible': True, 'name': 'exporter', 'plugins': [{'category': 'plugin', 'type': 'exporter', 'name': 'passthrough exporter', 'plugin': 'common_passthrough_publisher_exporter', 'visible': True, 'editable': True, 'options': {}, 'default_method': 'run'}]}], 'stage_order': ['collector', 'validator', 'exporter']}], 'finalizers': [{'category': 'step', 'type': 'finalizer', 'name': 'main', 'optional': False, 'enabled': True, 'selected': True, 'stages': [{'category': 'stage', 'type': 'pre_finalizer', 'visible': True, 'name': 'pre_finalizer', 'plugins': [{'category': 'plugin', 'type': 'pre_finalizer', 'name': 'pre to ftrack server', 'plugin': 'common_default_publisher_pre_finalizer', 'visible': False, 'editable': True, 'options': {}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'finalizer', 'visible': True, 'name': 'finalizer', 'plugins': [{'category': 'plugin', 'type': 'finalizer', 'name': 'to ftrack server', 'plugin': 'common_default_publisher_finalizer', 'visible': False, 'editable': True, 'options': {}, 'default_method': 'run'}]}, {'category': 'stage', 'type': 'post_finalizer', 'visible': True, 'name': 'post_finalizer', 'plugins': [{'category': 'plugin', 'type': 'post_finalizer', 'name': 'post to ftrack server', 'plugin': 'common_default_publisher_post_finalizer', 'visible': False, 'editable': True, 'options': {}, 'default_method': 'run'}]}], 'stage_order': ['pre_finalizer', 'finalizer', 'post_finalizer']}], '_config': {'type': 'config', 'engine_type': 'publisher'}}
+def_object = definition_object.DefinitionObject(definition)
+steps = def_object.steps
+plugin = steps[0].stages[0].plugins[0]
+options = plugin.options
+options.test
+options.keys()
+options.asset_name
+options.asset_name = 'testing'
+
+
+#doing it by our custom class the dictionary is not muttable
+from ftrack_connect_pipeline.definition import definition_object
+a = {'a': 1, 'context':[{'category':'step', 'info': [{'category':'stage','info': [{'a': 4}]}]}]}
+tt = definition_object.DefinitionObject(a)
+st = tt.steps[0]
+st['info']='qwe'
+tt
+
+
+#Doing it manually the dictionary is mutable
+a = {'a': 1, 'context':[{'category':'step', 'info': [{'category':'stage','info': [{'a': 4}]}]}]}
+b = a['context'][0]
+b['info']='qwe'
+a
+-'''
